68ee170b08676a6ebb54b965344a016929a7a53c
AndreasNicoT <108925729+AndreasNicoT@users.noreply.github.com>
2023-03-31 10:11:38 +0700

    Add files via upload

--- serviceWorker.js
+++ serviceWorker.js
@@ -59,4 +59,10 @@ self.addEventListener('install', function(event) {
   );
 });
 
-self.addEventListener
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+      caches.match(event.request).then(function(response) {
+          return response || fetch(event.request);
+      })
+  );
+});
